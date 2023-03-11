52efc43512db76311cc4c3b366f29ec5a8f90a70
celdering-trm <christian.eldering@trmlabs.com>
2023-03-10 22:08:18 -0500

    add chatgpt generated javadocs

--- src/main/java/MySong.java
+++ src/main/java/MySong.java
@@ -5,12 +5,23 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
+/**
+ * A class representing a song, with properties for title, artist, year, and album.
+ * Implements the Comparable interface to allow for sorting by title.
+ */
 public class MySong implements Comparable<MySong> {
     private String title;
     private String artist;
     private int year;
     private String album;
 
+    /**
+     * Constructs a new MySong object with the given properties.
+     * @param title the title of the song
+     * @param artist the artist who performed the song
+     * @param year the year the song was released
+     * @param album the album the song appears on
+     */
     public MySong(String title, String artist, int year, String album) {
         this.title = title;
         this.artist = artist;
@@ -18,22 +29,44 @@ public class MySong implements Comparable<MySong> {
         this.album = album;
     }
 
+    /**
+     * Returns the title of the song.
+     * @return the title of the song
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Returns the artist who performed the song.
+     * @return the artist who performed the song
+     */
     public String getArtist() {
         return artist;
     }
 
+    /**
+     * Returns the year the song was released.
+     * @return the year the song was released
+     */
     public int getYear() {
         return year;
     }
 
+    /**
+     * Returns the album the song appears on.
+     * @return the album the song appears on
+     */
     public String getAlbum() {
         return album;
     }
 
+    /**
+     * Checks if this MySong object is equal to another object.
+     * Two MySong objects are considered equal if they have the same title, artist, year, and album.
+     * @param o the object to compare to
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -45,11 +78,20 @@ public class MySong implements Comparable<MySong> {
                 Objects.equals(album, mySong.album);
     }
 
+    /**
+     * Computes a hash code for this MySong object based on its title, artist, year, and album.
+     * @return the hash code for this object
+     */
     @Override
     public int hashCode() {
         return Objects.hash(title, artist, year, album);
     }
 
+    /**
+     * Compares this MySong object to another MySong object based on their titles.
+     * @param other the MySong object to compare to
+     * @return a negative integer, zero, or a positive integer as this object is less than, equal to, or greater than the specified object.
+     */
     @Override
     public int compareTo(MySong other) {
         return this.title.compareTo(other.title);
--- src/main/java/SongList.java
+++ src/main/java/SongList.java
@@ -4,27 +4,47 @@ import java.util.List;
 import java.util.ListIterator;
 import Bridges.base.SLelement;
 
+/**
+ * A class representing a list of songs. Implements the List interface to allow for manipulation of the list.
+ */
 public class SongList implements List<MySong> {
     private SLelement<MySong> head;
     private int size;
     private String name;
 
+    /**
+     * Constructs a new SongList object with the given name.
+     * @param name the name of the playlist
+     */
     public SongList(String name) {
         this.name = name;
         head = null;
         size = 0;
     }
 
+    /**
+     * Returns the number of songs in the playlist.
+     * @return the number of songs in the playlist
+     */
     @Override
     public int size() {
         return size;
     }
 
+    /**
+     * Checks if the playlist is empty.
+     * @return true if the playlist is empty, false otherwise
+     */
     @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * Checks if the playlist contains a specific song.
+     * @param o the song to check for
+     * @return true if the playlist contains the song, false otherwise
+     */
     @Override
     public boolean contains(Object o) {
         if (!(o instanceof MySong)) {
@@ -41,11 +61,19 @@ public class SongList implements List<MySong> {
         return false;
     }
 
+    /**
+     * Returns an iterator over the songs in the playlist.
+     * @return an iterator over the songs in the playlist
+     */
     @Override
     public Iterator<MySong> iterator() {
         return new Iterator<MySong>();
     }
 
+    /**
+     * Returns an array containing all of the songs in the playlist.
+     * @return an array containing all of the songs in the playlist
+     */
     @Override
     public Object[] toArray() {
         Object[] array = new Object[size];
@@ -59,6 +87,12 @@ public class SongList implements List<MySong> {
         return array;
     }
 
+    /**
+     * Returns an array containing all of the songs in the playlist.
+     * If the array is larger than the playlist, the extra elements are set to null.
+     * @param a the array to fill with the songs
+     * @return an array containing all of the songs in the playlist
+     */
     @Override
     public <T> T[] toArray(T[] a) {
         if (a.length < size) {
@@ -77,6 +111,11 @@ public class SongList implements List<MySong> {
         return a;
     }
 
+    /**
+     * Adds a song to the playlist.
+     * @param song the song to add
+     * @return true if the song was added successfully, false otherwise
+     */
     @Override
     public boolean add(MySong song) {
         SLelement<MySong> newElem = new SLelement<>(song, head);
